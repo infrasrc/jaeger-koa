0f2ebc2aab2de520c635e7846cfe6a87f6c50cea
Rotem Levi <rotem.levi@ironsrc.com>
2020-01-30 16:24:42 +0200

    remove bluebird use native promise add appquarium jaeger-tracer package

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const Promise                       = require('bluebird');
-const TEN_MINUTES_IN_MS             = 10 * 60 * 1000;
-const { Tags, FORMAT_HTTP_HEADERS } = require('opentracing');
-const _                             = require('lodash');
-const compose                       = require('koa-compose');
-const Router                        = require('koa-router');
-const { Readable }                  = require('stream');
+const TEN_MINUTES_IN_MS = 10 * 60 * 1000;
+const _ = require('lodash');
+const compose = require('koa-compose');
+const { Readable } = require('stream');
 
+const Tracer = require('jaeger-tracer');
+const { Tags, FORMAT_HTTP_HEADERS, globalTracer } = Tracer.opentracing;
+const tracer = globalTracer();
 let _logger = null;
-let _tracer = null;
+
+if (!tracer) throw 'please set globalTracer before starting jaeger-koa';
 
 class StringStream extends Readable {
     constructor(str, options) {
@@ -25,14 +26,13 @@ class StringStream extends Readable {
 const _getBody = (ctx) => {
     const bodyStream = _.get(ctx, 'body', {});
     if ((typeof bodyStream.on !== 'function') && !(bodyStream instanceof Readable)) return Promise.resolve(bodyStream);
-    
     const chunks = []
     return new Promise((resolve, reject) => {
         bodyStream.on('data', chunk => chunks.push(chunk));
         bodyStream.on('error', reject);
         bodyStream.on('end', () => {
             const body = Buffer.concat(chunks).toString('utf8');
-            ctx.body   = new StringStream(body);
+            ctx.body = new StringStream(body);
             resolve(body);
         });
     });
@@ -41,7 +41,6 @@ const _getBody = (ctx) => {
 const _finishSpan = (ctx) => {
     const span = _.get(ctx, 'span', null);
     if (!span) throw 'span not found';
-
     const spanTimeout = _.get(ctx, 'spanTimeout', null);
     if (spanTimeout) clearTimeout(spanTimeout);
     span.finish();
@@ -57,23 +56,32 @@ const _setTimeout = (ctx) => {
     });
 }
 
+const _logError = (message, error) => {
+    if (_logger && _logger.error instanceof Function) _logger.error(message, error);
+}
+
 class KoaJaeger {
 
     constructor() {
         this.handler = this.handler.bind(this);
-        this.start   = this.start.bind(this);
-        this.end     = this.end.bind(this);
+        this.start = this.start.bind(this);
+        this.end = this.end.bind(this);
     }
 
     handler(...args) {
-        const routerIndex           = _.findIndex(args, (arg) => (arg.constructor.name === 'Router') ? true : false);
-        const router                = args[routerIndex];
-        let beforeMiddlewaresRouter = _.chain(args).slice(0, routerIndex).filter((arg) => (arg instanceof Function) ? true : false).value();
-        let afterMiddlewaresRouter = _.chain(args).slice(routerIndex + 1).filter((arg) => (arg instanceof Function) ? true : false).value();
-        _.each(router.stack, (v) => {
-            v.stack = [...beforeMiddlewaresRouter, compose([this.start, ...v.stack, this.end], ...afterMiddlewaresRouter)];
-        });
-        
+        let router = null;
+        try {
+            const routerIndex = _.findIndex(args, (arg) => (arg.constructor.name === 'Router') ? true : false);
+            router = args[routerIndex];
+            let beforeMiddlewaresRouter = _.chain(args).slice(0, routerIndex).filter((arg) => (arg instanceof Function) ? true : false).value();
+            let afterMiddlewaresRouter = _.chain(args).slice(routerIndex + 1).filter((arg) => (arg instanceof Function) ? true : false).value();
+            _.each(router.stack, (v) => {
+                v.stack = [...beforeMiddlewaresRouter, compose([this.start, ...v.stack, this.end], ...afterMiddlewaresRouter)];
+            });
+        } catch (error) {
+            _logError(error.message, error);
+            throw error;
+        }
         return router;
     };
 
@@ -81,38 +89,36 @@ class KoaJaeger {
         try {
             const span = _.get(ctx, 'span', null);
             if (!span) throw 'span not found';
-
             span.setTag(Tags.ERROR, true);
             span.setTag(Tags.HTTP_STATUS_CODE, ctx.status);
             span.log({
-                event  : 'error',
+                event: 'error',
                 message: error.message,
-                stack  : error.stack
+                stack: error.stack
             });
             _finishSpan(ctx);
         } catch (error) {
-            if (_logger) _logger.error(error.message, error);
+            _logError(error.message, error);
         }
     };
 
     async start(ctx, next) {
         try {
-            if (!_tracer) throw 'tracer not found'
-            const email             = _.get(ctx, 'state.user.email', null);
-            const params            = _.get(ctx, 'params', {});
-            const query             = _.get(ctx, 'query', null);
-            const method            = _.get(ctx, 'method', '');
-            const headers           = _.get(ctx, 'headers', null);
-            const routePath         = _.get(ctx, '_matchedRoute', '');
-            const name              = `${method} ${routePath.substring(1)}`;
-            const parentSpanContext = _tracer.extract(FORMAT_HTTP_HEADERS, headers);
-            const spanOptions       = {};
+            const email = _.get(ctx, 'state.user.email', null);
+            const params = _.get(ctx, 'params', {});
+            const query = _.get(ctx, 'query', null);
+            const method = _.get(ctx, 'method', '');
+            const headers = _.get(ctx, 'headers', null);
+            const routePath = _.get(ctx, '_matchedRoute', '');
+            const name = `${method} ${routePath.substring(1)}`;
+            const parentSpanContext = tracer.extract(FORMAT_HTTP_HEADERS, headers);
+            const spanOptions = {};
 
             if (parentSpanContext) spanOptions.childOf = parentSpanContext;
 
             _setTimeout(ctx);
 
-            ctx.span = _tracer.startSpan(name, spanOptions);
+            ctx.span = tracer.startSpan(name, spanOptions);
 
             ctx.span.setTag(Tags.SPAN_KIND, Tags.SPAN_KIND_RPC_SERVER);
 
@@ -127,13 +133,14 @@ class KoaJaeger {
             });
 
         } catch (error) {
-            if (_logger) _logger.error(error.message, error);
+            _logError(error.message, error);
             ctx.app.emit('error', error, ctx);
-        } return await next();
+        }
+        return next();
     };
 
     async end(ctx) {
-        try {
+        try {                        
             const span = _.get(ctx, 'span', null);
             if (!span) throw 'span not found';
 
@@ -145,21 +152,20 @@ class KoaJaeger {
             _finishSpan(ctx);
         } catch (error) {
             _finishSpan(ctx);
-            if (_logger) _logger.error(error.message, error);
+            _logError(error.message, error);
             ctx.app.emit('error', error, ctx);
         }
     };
 }
 
-module.exports = (Koa, logger, tracer) => {
-    _logger = logger;
-    _tracer = tracer;
+module.exports = (Koa, Logger) => {
+    _logger = Logger;
 
     const emit = Koa.prototype.emit;
 
     Koa.prototype.emit = function (...args) {
         const error = args[1];
-        args[1]     = (error instanceof Error) ? error : new Error(error);
+        args[1] = (error instanceof Error) ? error : new Error(error);
         return emit.call(this, ...args);
     };
 
